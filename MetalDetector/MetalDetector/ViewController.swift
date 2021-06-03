9df6b7e9f9658e201d63c5ee2a4540738b32f8d1
bwJeong <bwjeong93@gmail.com>
2021-06-03 18:07:15 +0900

    #2 MetalDetector

--- MetalDetector/MetalDetector/ViewController.swift
+++ MetalDetector/MetalDetector/ViewController.swift
@@ -10,18 +10,27 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
+    @IBOutlet weak var totalValueLabel: UILabel!
+    
     let magnetometerManager = MagnetometerManager()
     let disposeBag = DisposeBag()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         magnetometerManager.totalValue
-            .subscribe(onNext: { value in
-                print(value)
-            })
+            .observe(on: MainScheduler.instance)
+            .bind { [weak self] totalValue in
+                self?.view.backgroundColor = UIColor(
+                    red: totalValue > 500 ? 1.0 : CGFloat(totalValue / 500),
+                    green: totalValue > 500 ? 0 : CGFloat(1 - totalValue / 500),
+                    blue: 0,
+                    alpha: 1
+                )
+                self?.totalValueLabel.text = String(format: "%.1f", totalValue)
+            }
             .disposed(by: disposeBag)
-        
+
         magnetometerManager.startMagnetometer()
     }
 }
