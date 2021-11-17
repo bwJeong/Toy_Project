46fd6d6b0b590d42fae4485368ef6623320f2c4e
bwJeong <bwjeong93@gmail.com>
2021-11-17 19:29:56 +0900

    #3 FirebaseChat - LoginView 생성

---
+++ FirebaseChat/FirebaseChat/FirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatApp.swift
+//  FirebaseChat
+//
+//  Created by Byungwook Jeong on 2021/11/17.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
